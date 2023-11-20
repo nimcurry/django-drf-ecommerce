48156f213e2b0423af867c442ea8aedcdc52e69e
nimish <nimcurry@yahoo.co.in>
2023-11-20 14:51:39 -0500

    first commit

---
+++ drfecommerce/commands.cmd
@@ -0,0 +1,12 @@
+To create django project
+1. run virtual environment: python3 -m venv vene <virtual_environment_name>
+2. activate the virtual environment: source ~/venv/src/bin/activate
+3. install django project: django-admin startproject <project-name>
+4. to access python shell: python3 manage.py shell
+5. to run the django project: python3 manage.py runserver
+6. pip3 install -r requirements.txt
+#python-dotenv is used to set up configuration variables
+7. to install a .env file: use pip3 install python-dotenv
+8. to install django rest framework: pip3 install djangorestframework
+9. to install python unit testing framework - pip3 install pytest
+10. for django testing: pip3 install pytest-django
---
+++ drfecommerce/drfecommerce/settings/production.py
@@ -0,0 +1,7 @@
+from .base import *
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+SECRET_KEY='yed#--2j%@&cbou0(%m-i025i1-r52-$_1--^+2!6+hqoc_cys'
